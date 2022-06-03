0d7b45b2d490a5af9a788c4c4d2521cd4d0b2699
Moshin <bsef19m031@pucit.edu.pk>
2022-06-03 10:15:54 +0500

    learn screen added

--- app/src/main/java/com/example/learncolors/MainActivity.java
+++ app/src/main/java/com/example/learncolors/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.learncolors;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button1 = (Button) findViewById(R.id.learnbtn);
+        Button button2 = (Button) findViewById(R.id.practicebtn);
+        button1.setOnClickListener((View.OnClickListener) this);
+        button2.setOnClickListener((View.OnClickListener) this);
     }
+
+    // Todo pta nai yahan kya aana he
+    // @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.learnbtn:
+                Intent intent1 = new Intent(this, LearnMainScreen.class);
+                startActivity(intent1);
+                break;
+            case R.id.practicebtn:
+                Intent intent2 = new Intent(this, AnotherActivity.class);
+                startActivity(intent2);
+                break;
+            default:
+                break;
+        }
 }
