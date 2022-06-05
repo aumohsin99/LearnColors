ed56a22dfa4aba8cd8e2e9f70f9c5f9ece77d657
Moshin <bsef19m031@pucit.edu.pk>
2022-06-05 20:22:03 +0500

    rand function added

--- app/src/main/java/com/example/learncolors/PracticeMainScreen.java
+++ app/src/main/java/com/example/learncolors/PracticeMainScreen.java
@@ -4,48 +4,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.TextView;
+import java.lang.Math;
 
 public class PracticeMainScreen extends AppCompatActivity {
 
+    int min = 1;
+    int max= 12;
+    int index = (int)(Math.random()*(max-min+1)+min);
+
    // int index = 0;
 
-    int index = 5;
+   //  int index = 5;
    // String colorsarray[];
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practice_main_screen);
 
         TextView questionheadingview = findViewById(R.id.questionheading);
-        questionheadingview.setText("Select button in "+colorsarray[index] + " color:");
+        questionheadingview.setText("Select button in "+ colorsarray[index] + " color:");
     }
 
     String[] colorsarray = {"Red", "Green", "Yellow", "Black", "Blue", "Brown", "Gray", "Orange", "Pink", "Purple", "White" };
 
-    /*
-    public void randcolorgenerator(String colorsarry[])
-    {
-        int i = 0;
-        int j = 11;
 
-     int index = random(i, j);
-    }
-
-
-    private int randcolorgenerator(int i, int j)
-    {
-        int index = random(i,j);
-    }
-   */
 
     String hell = "bye";
 
 
 
-    //   int testarray[]={};
-//   testarray[].
-   // public void random
+
 
 }
